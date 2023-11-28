6ef3ebae96aaf973911fa86289abe2015eccf7f8
VladislavSidenko <vladislav.sidenko214@gmail.com>
2023-11-28 15:35:25 +0200

    Add Lab3. Adapter and Proxy Patterns

---
+++ Lab3/Lab3_1/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_1
+{
+    internal class Character
+    {
+        public void MoveCharacter()
+        {
+            Console.WriteLine("The character is walking");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Lab 2_1 UML Abstract Factory Pattern:
 
 Lab 2_2 UML Builder Pattern:
 ![Alt text](image-1.png)
+
+Lab 3_1 UML Adapter Pattern:
+![Alt text](image-2.png)
+
+Lab 3_2 UML Proxy Pattern:
+![Alt text](image-3.png)
